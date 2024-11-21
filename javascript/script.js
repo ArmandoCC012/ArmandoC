fbeaaa9690ecb5d2472d4356087e1dd723d21449
ArmandoCC012 <armandocallapa@gmail.com>
2024-11-21 17:49:49 -0400

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,20 @@
+    const d=document.getElementById("button");
+    d.addEventListener('click', (e) =>{
+     
+    e.preventDefault();
+    const n=document.getElementById("nom").value;
+    const c=document.getElementById("con").value;
+    const mi_nombre="Armando";
+    const mi_contra="61912988";
+
+    if(n==mi_nombre && c==mi_contra) 
+        {
+        window.location.href="menu_enlace/inicio.html"; 
+        } 
+    else 
+    {
+        document.getElementById("mensaje_de_error").textContent="Usuario o contraseña incorrectos.";
+    }
+
+    });
+    
